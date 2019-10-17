903b66a12da80c144ff8076090d392baa9a3da84
dollerbill <billycropley@gmail.com>
2019-10-16 23:48:36 -0700

    Initial commit

---
+++ RFI_Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFI_Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ RFI_Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RFI_Engine.Models;
+
+namespace RFI_Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Bilbo";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.Experience = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
